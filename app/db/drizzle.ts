41cae75a4265e69143fb67d8f854228807db7df7
Italo Canturil <italo.cantur@gmail.com>
2025-04-28 15:52:47 -0300

    refactor(db): reorganize database schema and drizzle setup

--- app/db/drizzle.ts
+++
@@ -1,7 +0,0 @@
-// Make sure to install the 'postgres' package
-import { drizzle } from "drizzle-orm/postgres-js";
-
-const db = drizzle(process.env.DATABASE_URL);
-
-const result = await db.execute("select 1");
-console.log("🚀 ~ result:", result);
