1f50dd9cff9b63936c171f4d8d2b47a3311526d3
Italo Canturil <italo.cantur@gmail.com>
2025-04-28 15:52:08 -0300

    feat(TransactionService): add service to handle expense transactions

---
+++ app/_services/TransactionService.ts
@@ -0,0 +1,58 @@
+import { auth } from "@clerk/nextjs/server";
+import { expenseTable } from "../_db/schema";
+import db from "../_db/drizzle";
+import { and, eq } from "drizzle-orm";
+
+interface ExpenseParams {
+    category: string;
+    amount: number;
+}
+export class TransactionService {
+    async processWebRequest (params: ExpenseParams) {
+        const { userId } = await auth();
+
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+    }
+
+    async addExpense (params: ExpenseParams) {
+        const { userId } = await auth();
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        await db
+            .insert(expenseTable)
+            .values({ ...params, amount: params.amount.toString(), user_id: userId });
+    }
+
+    async removeExpense  (params: ExpenseParams) {
+        const { userId } = await auth();
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+    
+        await db
+            .delete(expenseTable)
+            .where(
+                and(
+                    eq(expenseTable.user_id, userId),
+                    eq(expenseTable.category, params.category),
+                    eq(expenseTable.amount, params.amount.toString())
+                )
+            );
+    };
+
+    async getExpenses () {
+        const { userId } = await auth();
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        await db
+            .select()
+            .from(expenseTable)
+            .where(eq(expenseTable.user_id, userId))
+    }
+}
