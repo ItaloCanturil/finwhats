32687d35b97276e04a5fc4f0125281ec5f5873e3
Italo Canturil <italo.cantur@gmail.com>
2025-05-07 21:49:17 -0300

    feat(payment): add Stripe integration and webhook handlers

---
+++ app/_lib/stripe.ts
@@ -0,0 +1,10 @@
+import Stripe from 'stripe';
+
+if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Stripe secret key is missing");
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: "2025-04-30.basil"
+});
+export default stripe;
---
+++ app/api/v1/webhooks/stripe/route.ts
@@ -0,0 +1,33 @@
+import stripe from "@/app/_lib/stripe";
+import { NextResponse } from "next/server"
+
+export const POST = async (request: Request) => {
+   try {
+    const body = await request.text();
+    const signature = request.headers.get("stripe-signature");
+
+    const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+
+    switch (event.type) {
+        case "checkout.session.completed":
+            if(event.data.object.payment_status === "paid") {
+                // TODO: Add code here
+            }
+
+            if (event.data.object.payment_status === "unpaid") {
+                // TODO: Add code here
+            }
+            break;
+        case "checkout.session.expired":
+            if (event.data.object.payment_status === "unpaid") {
+                
+            }
+            break;
+    }
+
+    return NextResponse.json({ result: event, ok: true });
+   } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: `Webhook error: ${error}`, ok: false }, { status: 500 });
+   }
+}
