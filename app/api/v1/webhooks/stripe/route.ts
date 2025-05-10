8c779f3b41f2e8c845463da8c20270efd873a83e
Italo Canturil <italo.cantur@gmail.com>
2025-05-10 05:53:58 -0300

    fix(webhooks): add validation for stripe signature and webhook secret

--- app/api/v1/webhooks/stripe/route.ts
+++ app/api/v1/webhooks/stripe/route.ts
@@ -6,6 +6,10 @@ export const POST = async (request: Request) => {
     const body = await request.text();
     const signature = request.headers.get("stripe-signature");
 
+    if (!signature || !process.env.STRIPE_WEBHOOK_SECRET) {
+      throw new Error('Missing stripe signature or webhook secret');
+    }
+    
     const event = stripe.webhooks.constructEvent(body, signature, process.env.STRIPE_WEBHOOK_SECRET);
 
     switch (event.type) {
