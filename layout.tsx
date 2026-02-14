5012c4def48ecb1ff287733cb5fda144c7b5b43a
ItaloCanturil <italo.cantur@gmail.com>
2026-02-13 21:57:08 -0300

    chore: Remove ClerkProvider from RootLayout.

--- layout.tsx
+++ layout.tsx
@@ -1,4 +1,3 @@
-import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 
 export default function RootLayout({
@@ -9,9 +8,7 @@ export default function RootLayout({
 	return (
 		<html lang="pt-BR">
 			<body>
-				<ClerkProvider>
-					<div className="flex h-full flex-col overflow-hidden">{children}</div>
-				</ClerkProvider>
+				<div className="flex h-full flex-col overflow-hidden">{children}</div>
 			</body>
 		</html>
 	);
